afe577a9c45ce2777f31c7d662da94b5f93f862f
Nikita Doroshenko <nikitokd05@gmail.com>
2025-06-18 22:42:13 +0300

    task solution

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -2,22 +2,27 @@ import './PostInfo.scss';
 import { UserInfo } from '../UserInfo';
 import { CommentList } from '../CommentList';
 
-export const PostInfo = ({ post, comments, users }) => (
-  <div className="PostInfo">
-    <div className="PostInfo__header">
-      <h3 className="PostInfo__title">{post.title}</h3>
-      <p>
-        <UserInfo user={users} />
-      </p>
+export const PostInfo = ({ post, comments, users }) => {
+  const user = post.user || users;
+  const postComments = post.comments || comments;
+
+  return (
+    <div className="PostInfo">
+      <div className="PostInfo__header">
+        <h3 className="PostInfo__title">{post.title}</h3>
+        <p>
+          <UserInfo user={user} />
+        </p>
+      </div>
+      <p className="PostInfo__body">{post.body}</p>
+      {postComments && postComments.length > 0 ? (
+        <CommentList comments={postComments} />
+      ) : (
+        <>
+          <hr />
+          <b data-cy="NoCommentsMessage">No comments yet</b>
+        </>
+      )}
     </div>
-    <p className="PostInfo__body">{post.body}</p>
-    {comments !== null ? (
-      <CommentList comments={comments} />
-    ) : (
-      <>
-        <hr />
-        <b data-cy="NoCommentsMessage">No comments yet</b>
-      </>
-    )}
-  </div>
-);
+  );
+};
--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -2,8 +2,20 @@ import { PostInfo } from '../PostInfo';
 
 export const PostList = ({ posts, comments, users }) => (
   <div className="PostList">
-    {posts.map(post => (
-      <PostInfo post={post} comments={comments} users={users} key={post.id} />
-    ))}
+    {posts.map(post => {
+      const user = users.find(u => u.id === post.userId);
+      const postComments = comments.filter(
+        comment => comment.postId === post.id,
+      );
+
+      return (
+        <PostInfo
+          post={post}
+          users={user}
+          comments={postComments.length > 0 ? postComments : null}
+          key={post.id}
+        />
+      );
+    })}
   </div>
 );
